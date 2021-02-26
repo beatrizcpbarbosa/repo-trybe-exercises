7931e54430fde40120443f56b07c1840fc3ac728
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-02-26 09:57:48 -0300

    exercicios 2 e 3

--- fundamentos-desenvolvimento-web/bloco_04/dia_01/scripts.js
+++ fundamentos-desenvolvimento-web/bloco_04/dia_01/scripts.js
@@ -2,6 +2,7 @@
 
 let a = 5;
 let b = 10;
+let c = 15;
 
 let soma = a + b;
     console.log(soma);
@@ -18,4 +19,23 @@ let divsão = b / a;
 let modulo = b % a;
     console.log(modulo);
 
-// 
+// exercicio 2
+
+if (a > b) {
+    console.log('a é maior')
+} else if (b > a) {
+    console.log('b é maior')
+} else {
+    console.log('empate')
+}
+
+// exercicio 3
+
+if (a > b && a > c) {
+    console.log('a é maior')
+} else if (b > a && b > c) {
+    console.log('b é maior')
+} else f {
+    con
+}
+
