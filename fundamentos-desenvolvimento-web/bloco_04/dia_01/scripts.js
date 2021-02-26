122ef33acf8f9a8951fef8640a30676bca9ea90e
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-02-26 10:47:38 -0300

    exercicios 4,5,6

--- fundamentos-desenvolvimento-web/bloco_04/dia_01/scripts.js
+++ fundamentos-desenvolvimento-web/bloco_04/dia_01/scripts.js
@@ -36,6 +36,50 @@ if (a > b && a > c) {
 } else if (b > a && b > c) {
     console.log('b é maior')
 } else f {
-    con
+    console.log('c é o maior')
 }
 
+//exercicio 4
+
+if (a > 0) {
+    console.log('positive')
+} else if(a <0) {
+    console.log('negative')
+} else {
+    console.log('zero')
+}
+
+//exercicio 5
+
+if (a + b + c === 180){
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+// exercicio 6
+
+let peçaXadrez = 'rainha';
+
+switch (peçaXadrez.toLocaleLowerCase()) {}
+  case 'rei':
+    console.log('rei faz tal movimento')
+    break;
+  case 'dama':
+    console.log('dama faz tal movimento')
+    break;
+  case 'torre':
+    console.log('torre faz tal movimento')
+    break;
+  case 'rainha':
+    console.log('rainha faz tal movimento')
+    break;
+  case 'cavalo':
+    console.log('cavalo faz tal movimento')
+    break;  
+  default:
+    console.log('não existe essa peça')
+}    
+
+
+// exercicio 7
