e619d255214e89d767de853b8a0ea534ca8269ec
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-02-28 17:04:27 -0300

    exercicio 1 e 2

--- fundamentos-desenvolvimento-web/bloco_04/dia_02/tempCodeRunnerFile.js
+++ fundamentos-desenvolvimento-web/bloco_04/dia_02/tempCodeRunnerFile.js
@@ -1,14 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// exercicio 1
-
-for (index = 0; index < numbers.length; index =+ 1){
-    console.log(numbers[index])
-}
-
-// exercicio 2
-
-
 let soma = 0;
 
 for (index = 0; index < numbers.length; index =+ 1){
@@ -16,6 +7,3 @@ for (index = 0; index < numbers.length; index =+ 1){
 }
 
 console.log(soma);
-
-// exercicio 3
-
