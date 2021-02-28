f6e7f53962c72f38bc675c5be6ddc91618780d57
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-02-28 17:56:03 -0300

    exercicios 3 e 4

--- fundamentos-desenvolvimento-web/bloco_04/dia_02/scripts.js
+++ fundamentos-desenvolvimento-web/bloco_04/dia_02/scripts.js
@@ -8,14 +8,24 @@ for (index = 0; index < numbers.length; index =+ 1){
 
 // exercicio 2
 
-
 let soma = 0;
 
 for (index = 0; index < numbers.length; index =+ 1){
-    soma = (numbers[index] =+ 1);
+    soma =+ numbers[index];
 }
 
 console.log(soma);
 
 // exercicio 3
+
+let mediaAritimetica = soma/2;
+console.log(mediaAritimetica);
+
+// exercicio 4
+
+if ( mediaAritimetica > 20){
+    console.log('valor maior que 20');
+} else {
+    console.log('valor menor ou igual a 20')
+}
 
