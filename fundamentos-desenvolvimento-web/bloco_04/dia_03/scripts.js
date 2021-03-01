db376e61b3b7ddd7893602f22a8d0c51b13cb865
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-01 20:22:31 -0300

    bloco 3 exercicio 1

---
+++ fundamentos-desenvolvimento-web/bloco_04/dia_03/scripts.js
@@ -0,0 +1,13 @@
+// exercicio 1
+
+let n = 8;
+let estrela = '*';
+let imput = ' ';
+
+for (index = 0; index < n; index += 1){
+    imput += estrela;
+};
+
+for(let index = 0; index < n; index += 1){
+    console.log(imput);
+};
