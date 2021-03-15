b4ae2250561a52135a4bb0e7da2f60178e2dddfe
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-15 19:40:08 -0300

    adiciona parte html e javascript

---
+++ fundamentos-desenvolvimento-web/bloco_06/dia_01/script.js
@@ -0,0 +1,16 @@
+function CriarEstados (){
+    let inputEstado = document.querySelector('#estado');
+    let estados = ['Selecione seu estado', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+    for (let index = 0; index < estados.length; index += 1) {
+        let option = document.createElement('option');
+        inputEstado.appendChild(option).innerText = estados[index];
+        inputEstado.appendChild(option).value = estados[index];
+      }
+}
+
+CriarEstados ();
+
+function VerificaData (){
+    
+}
+
